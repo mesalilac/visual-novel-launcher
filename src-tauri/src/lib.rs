eee9bcd0bf5828f6646d55aad15cd39512897757
Abdalrzag Eisa <bmjfdrh@gmail.com>
2026-01-16 23:49:15 +0300

    feat: add `Single Instance` plugin

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -2,6 +2,8 @@ mod commands;
 mod database;
 mod services;
 
+use tauri::Manager;
+
 // Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
 #[tauri::command]
 fn greet(name: &str) -> String {
@@ -11,6 +13,12 @@ fn greet(name: &str) -> String {
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     tauri::Builder::default()
+        .plugin(tauri_plugin_single_instance::init(|app, _args, _cwd| {
+            let _ = app
+                .get_webview_window("main")
+                .expect("no main window")
+                .set_focus();
+        }))
         .plugin(tauri_plugin_opener::init())
         .invoke_handler(tauri::generate_handler![greet])
         .run(tauri::generate_context!())
