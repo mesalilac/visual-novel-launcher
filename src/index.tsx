77996739c390c2132966f0c0fa4e47581de33db5
Abdalrzag Eisa <bmjfdrh@gmail.com>
2026-02-06 02:46:42 +0300

    add custom font

--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,7 @@
 import { render } from 'solid-js/web';
 import App from './App';
 
+import './fonts.css';
 import './index.css';
 
 render(() => <App />, document.getElementById('root') as HTMLElement);
