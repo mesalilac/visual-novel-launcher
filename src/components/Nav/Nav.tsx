95e2d14ea53f58390045dae36fcee83223510d3c
Abdalrzag Eisa <bmjfdrh@gmail.com>
2026-02-12 20:23:40 +0300

    add missing nav bar elements

--- src/components/Nav/Nav.tsx
+++ src/components/Nav/Nav.tsx
@@ -1,5 +1,22 @@
+import { useGlobalData } from '@store';
+
 import './Nav.css';
+import { IconAddPlus, IconSettings } from '../icons';
 
 export const Nav = () => {
-    return <nav>nav</nav>;
+    const globalData = useGlobalData();
+    const vns = globalData.resources.vns;
+
+    return (
+        <nav class='flex-row justify-between'>
+            <h2>Visual Novel library ({vns.get()?.length})</h2>
+            <div class='flex-row gap-lg'>
+                <button class='button-primary' type='button'>
+                    <IconAddPlus />
+                    Add a Game
+                </button>
+                <IconSettings class='cursor-pointer settings-icon' />
+            </div>
+        </nav>
+    );
 };
