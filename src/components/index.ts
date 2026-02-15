e3dd1fbaec1f64facf5e96c5f47d48649300cb79
Abdalrzag Eisa <bmjfdrh@gmail.com>
2026-02-15 20:14:56 +0300

    rename component

--- src/components/index.ts
+++ src/components/index.ts
@@ -7,4 +7,4 @@ export * from './Modal/Modal';
 export * from './Nav/Nav';
 export * from './SettingsModal/SettingsModal';
 export * from './TagsManager/TagsManager';
-export * from './Visualnovel/Visualnovel';
+export * from './VisualNovel/VisualNovel';
