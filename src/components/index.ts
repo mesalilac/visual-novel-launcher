943c7f60d06e27c9e7a67febeda9371a020d78e5
Abdalrzag Eisa <bmjfdrh@gmail.com>
2026-02-15 20:18:50 +0300

    update index.ts

--- src/components/index.ts
+++ src/components/index.ts
@@ -7,4 +7,3 @@ export * from './Modal/Modal';
 export * from './Nav/Nav';
 export * from './SettingsModal/SettingsModal';
 export * from './TagsManager/TagsManager';
-export * from './VisualNovel/VisualNovel';
