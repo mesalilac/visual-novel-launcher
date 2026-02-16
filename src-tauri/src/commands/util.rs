31a815c0e9bc85df24169b9b81c45feb76192aa3
Abdalrzag Eisa <bmjfdrh@gmail.com>
2026-02-17 02:43:19 +0300

    rename ipc command

--- src-tauri/src/commands/util.rs
+++ src-tauri/src/commands/util.rs
@@ -68,7 +68,7 @@ pub async fn util_sync_library(state: AppState<'_>) -> CommandResult<Vec<VisualN
 #[tauri::command]
 #[auto_collect_command]
 #[specta::specta]
-pub async fn util_open_folder(state: AppState<'_>, path: String) -> CommandResult<()> {
+pub async fn util_open_path(_state: AppState<'_>, path: String) -> CommandResult<()> {
     tauri_plugin_opener::open_path(path, None::<&str>)?;
 
     Ok(())
