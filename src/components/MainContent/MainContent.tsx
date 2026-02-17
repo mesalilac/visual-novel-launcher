cc436a2c142cf9168b9695b8d73e2cad38b9106b
Abdalrzag Eisa <bmjfdrh@gmail.com>
2026-02-17 21:05:01 +0300

    refetch playSessions

--- src/components/MainContent/MainContent.tsx
+++ src/components/MainContent/MainContent.tsx
@@ -43,6 +43,7 @@ export const MainContent = () => {
                     });
                 });
                 globalData.resources.generalStats.refetch();
+                globalData.resources.playSessions.refetch();
             }
         });
     });
