28954efa6f1f05e9c9dd771106ed3b135e03c40f
Abdalrzag Eisa <bmjfdrh@gmail.com>
2026-02-22 01:04:33 +0300

    resolve cover path in update

--- src-tauri/src/commands/update.rs
+++ src-tauri/src/commands/update.rs
@@ -1,10 +1,14 @@
+use std::path::{Path, PathBuf};
+
 use super::prelude::*;
 use crate::utils::db::normalize_optional_string;
 use crate::APP_SETTINGS_ID;
 use diesel::{
+    associations::HasTable,
     dsl::{delete, exists, select},
     insert_into, update,
 };
+use nanoid::nanoid;
 
 #[derive(Debug, Clone, Serialize, AsChangeset)]
 #[diesel(table_name = schema::visual_novels)]
@@ -21,6 +25,35 @@ struct VisualNovelChangeset {
     pub launch_options: Option<String>,
 }
 
+fn resolve_cover_path(cover_path: Option<String>, vn_dir_path: String) -> Option<String> {
+    let source_path_str = cover_path.as_ref()?;
+
+    let source_path = PathBuf::from(source_path_str);
+    let vn_dir_path = PathBuf::from(&vn_dir_path);
+
+    if source_path.parent() == Some(&vn_dir_path) {
+        return cover_path;
+    }
+
+    let ext = source_path
+        .extension()
+        .and_then(|s| s.to_str())
+        .unwrap_or("png");
+
+    let new_file_name = format!("{}.{}", nanoid!(), ext);
+
+    let dest_path = vn_dir_path.join(new_file_name);
+
+    if dest_path.exists() {
+        return cover_path;
+    }
+
+    match std::fs::copy(source_path, &dest_path) {
+        Ok(_) => Some(dest_path.to_string_lossy().to_string()),
+        Err(_) => cover_path,
+    }
+}
+
 #[tauri::command]
 #[auto_collect_command]
 #[specta::specta]
@@ -35,10 +68,17 @@ pub async fn update_visual_novel(
 
     let mut conn = state.pool.get()?;
 
+    let vn_dir_path = VisualNovelEntity::table()
+        .find(&id)
+        .get_result::<VisualNovelEntity>(&mut conn)?
+        .dir_path;
+
+    let cover_path: Option<String> = resolve_cover_path(payload.cover_path, vn_dir_path);
+
     let vn_changeset = VisualNovelChangeset {
         title: payload.title.map(|s| s.trim().into()),
         description: normalize_optional_string(payload.description),
-        cover_path: normalize_optional_string(payload.cover_path),
+        cover_path: normalize_optional_string(cover_path),
         playtime: payload.playtime,
         status: payload.status,
         is_favorite: payload.is_favorite,
