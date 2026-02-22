faf7441aca206dbfaacd9a016e49d59dbbbe822d
Abdalrzag Eisa <bmjfdrh@gmail.com>
2026-02-22 09:20:40 +0300

    fix primary key

--- src-tauri/src/database/entities/play_session.rs
+++ src-tauri/src/database/entities/play_session.rs
@@ -4,7 +4,6 @@ use super::VisualNovelEntity;
 #[derive(Queryable, Selectable, Insertable, Identifiable, Associations, Debug, Clone)]
 #[diesel(belongs_to(VisualNovelEntity, foreign_key = visual_novel_id))]
 #[diesel(table_name = play_sessions)]
-#[diesel(primary_key(visual_novel_id))]
 pub struct PlaySessionEntity {
     pub id: String,
     pub visual_novel_id: String,
