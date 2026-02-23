87713be8b0d34434d0fcf616ce30d87953c631a2
Abdalrzag Eisa <bmjfdrh@gmail.com>
2026-02-23 06:05:09 +0300

    remove comment

--- src/components/VisualNovelCard/VisualNovelCard.tsx
+++ src/components/VisualNovelCard/VisualNovelCard.tsx
@@ -286,7 +286,6 @@ export const VisualNovelCard = (props: { vn: VisualNovel }) => {
                     </button>
                 </div>
             </Popover>
-            {/* FIXME: This creates a modal for every vn. it should be a single global edit modal */}
             <Show when={showEditModal()}>
                 <Modal isOpen={showEditModal} setIsOpen={setShowEditModal}>
                     <EditVisualNovelModal vn={props.vn} />
