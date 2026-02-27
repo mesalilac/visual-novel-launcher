cd304bf6caf665822dab4e4d7fe08bc20d89dc48
Abdalrzag Eisa <bmjfdrh@gmail.com>
2026-02-27 08:30:34 +0300

    add width and height to divider

--- src/components/Divider/Divider.tsx
+++ src/components/Divider/Divider.tsx
@@ -1,12 +1,24 @@
+import type { VoidComponent } from 'solid-js';
 import './Divider.css';
 
-export const Divider = (props: { class?: string; vertical?: boolean }) => {
+type Props = {
+    class?: string;
+    vertical?: boolean;
+    width?: string;
+    height?: string;
+};
+
+export const Divider: VoidComponent<Props> = (props) => {
     return (
         <div
             class={props.class}
             classList={{
                 divider: !props.vertical,
                 'divider-vertical': props.vertical,
+            }}
+            style={{
+                width: props.width,
+                height: props.height,
             }}
         />
     );
