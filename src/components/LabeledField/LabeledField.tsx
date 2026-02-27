74c44fe6d7d5f73ee019338adc6d179ac4548c8d
Abdalrzag Eisa <bmjfdrh@gmail.com>
2026-02-27 07:36:55 +0300

    add inline prop

--- src/components/LabeledField/LabeledField.tsx
+++ src/components/LabeledField/LabeledField.tsx
@@ -6,13 +6,17 @@ type Props = {
     name: string;
     title?: string;
     icon?: JSX.Element;
+    inline?: boolean;
     ref?: HTMLDivElement | ((el: HTMLDivElement) => void);
     children: JSX.Element;
 };
 
 export const LabeledField: ParentComponent<Props> = (props: Props) => {
     return (
-        <div class='flex-column flex-1' ref={props.ref}>
+        <div
+            class={`flex-${props.inline ? 'row' : 'column'} flex-1`}
+            ref={props.ref}
+        >
             <h4 class='flex-row' title={props.title}>
                 {props.icon}
                 {toTitleCase(props.name)}:
